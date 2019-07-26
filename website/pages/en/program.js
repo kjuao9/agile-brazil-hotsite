34ebe6c409eef9b06bc856604e3025cbaa595d58
Ceci Fernandes <cecilia.fernandes@nubank.com.br>
2019-07-25 21:17:38 -0300

    [WIP] adding program sessions to program page

--- website/pages/en/program.js
+++ website/pages/en/program.js
@@ -7,7 +7,7 @@ const MarkdownBlock = CompLibrary.MarkdownBlock; /* Used to read markdown */
 const Container = CompLibrary.Container;
 const GridBlock = CompLibrary.GridBlock;
 
-class HomeSplash extends React.Component {
+class ProgramSessions extends React.Component {
   render() {
     const {siteConfig, language = ''} = this.props;
     const {baseUrl, docsUrl} = siteConfig;
@@ -15,54 +15,14 @@ class HomeSplash extends React.Component {
     const langPart = `${language ? `${language}/` : ''}`;
     const docUrl = doc => `${baseUrl}${docsPart}${langPart}${doc}`;
 
-    const SplashContainer = props => (
-      <div className="homeContainer">
-        <div className="homeSplashFade">
-          <div className="wrapper homeWrapper">{props.children}</div>
-        </div>
-      </div>
-    );
-
-    const ProjectTitle = () => (
-      <h2 className="projectTitle">
-        {siteConfig.title}
-        <small>{siteConfig.city}, <b>{siteConfig.year}</b></small>
-        <small><translate>september 11, 12 and 13</translate></small>
-      </h2>
-    );
-
-    const Logo = props => (
-      <div className="projectLogo">
-        <img src={props.img_src} alt="Project Logo" />
-      </div>
-    );
-
-    const PromoSection = props => (
-      <div className="section promoSection">
-        <div className="promoRow">
-          <div className="pluginRowBlock">{props.children}</div>
-        </div>
-      </div>
-    );
-
-    const Button = props => (
-      <div className="pluginWrapper buttonWrapper">
-        <a className="button" href={props.href} target={props.target}>
-          {props.children}
-        </a>
-      </div>
+    const ApprovedSessions = props => (
+      <ul class="approvedSessions">
+        <li>Something</li>
+      </ul>
     );
 
     return (
-      <SplashContainer>
-        <Logo img_src={`${baseUrl}img/logo-ab.svg`} />
-        <div className="inner">
-          <ProjectTitle siteConfig={siteConfig} />
-          <PromoSection>
-            <Button href="#register"><translate>Register now!</translate></Button>
-          </PromoSection>
-        </div>
-      </SplashContainer>
+      <ApprovedSessions />
     );
   }
 }
@@ -74,7 +34,7 @@ class Program extends React.Component {
 
     const Introduction = () => (
       <section className="program--introduction">
-        <h1 className="program--introduction-title"><translate>How works the program</translate> <a href="http://agilebrazil.com" target="_blank">#AgileBR</a></h1>
+        <h1 className="program--introduction-title"><translate>How do we build the the program</translate> <a href="http://agilebrazil.com" target="_blank">#AgileBR</a></h1>
 
         <p className="program--description"><translate>Making the Agile Brazil Program is a journey that starts the day after the previous Agile Brazil and ends about a month after the event happens. It's a long road and many community volunteers are involved in it.</translate></p>
       </section>
@@ -105,7 +65,7 @@ class Program extends React.Component {
     );
 
     const TimeLineEvents = () => (
-      <section className="darkBackground">
+      <section className="lightBackground">
         <section className="program--timeline-events">
         <h2><translate>Before opening submissions</translate>...</h2>
         <ul>
@@ -301,7 +261,7 @@ class Program extends React.Component {
 
     return (
       <main>
-        <HomeSplash siteConfig={siteConfig} language={language} />
+        <ProgramSessions />
         <div className="mainContainer program--container">
           <Introduction />
           <WhoWeAre />
